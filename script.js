16dbf718777112e3b0f837a8640904b1dea25d0b
lisandroseia <lisandroseia11@gmail.com>
2021-12-13 18:41:45 -0300

    Add scripting to mobile menu

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const menuButton = document.querySelector('.hamburger-btn');
 const menu = document.querySelector('.nav-ul');
+const menuItem = document.querySelectorAll('.nav-li');
 
-menuButton.addEventListener('click', ()=>{
+menuButton.addEventListener('click', toggleMenu)
+
+menuItem.forEach((item) => {
+  item.addEventListener('click', toggleMenu)
+})
+
+function toggleMenu(){
   const visibility = menu.getAttribute('data-visible');
   if(visibility==="false"){
     menu.setAttribute('data-visible', true);
+    menuButton.setAttribute('aria-expanded', true)
   } else {
     menu.setAttribute('data-visible', false);
+    menuButton.setAttribute('aria-expanded', false)
   }
-})
+}
