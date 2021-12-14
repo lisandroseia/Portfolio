c6e8cd338f1045f7bdb0073b3381f7b56cfab9bc
lisandroseia <lisandroseia11@gmail.com>
2021-12-14 19:20:49 -0300

    Start popup window

--- script.js
+++ script.js
@@ -25,3 +25,56 @@ menuItem.forEach((item) => {
     }
   });
 });
+
+/* modal windows */
+
+const popupBtn = document.querySelectorAll('.link-btn');
+const popupContainer = document.querySelector('.modal-container');
+const closeBtn = document.querySelector('.popup-btn')
+
+popupBtn.forEach((item) => {
+  item.addEventListener('click', () => {
+      let project = item.getAttribute('data-value')
+      console.log(pages[project].title)
+      popupContainer.setAttribute('data-visible', true)
+  })
+})
+
+closeBtn.addEventListener('click', () => {
+  popupContainer.setAttribute('data-visible', false)
+})
+
+const titles = [];
+
+document.querySelectorAll('div.work-info > h2').forEach((item) => {
+  titles.push(item.textContent);
+});
+
+const descriptions = [];
+
+document.querySelectorAll('.work-text').forEach((item) => {
+  descriptions.push(item.textContent);
+})
+
+const technologies = [];
+const techs = []
+document.querySelectorAll('.framework').forEach((item) => {
+  techs.push(item.textContent)
+});
+technologies.push(techs.slice(0,3))
+technologies.push(techs.slice(3,7))
+technologies.push(techs.slice(7,11))
+technologies.push(techs.slice(11,15))
+
+const pages = {};
+for(let i = 0; i < titles.length; i++){
+  pages[`project${i}`] = {};
+  pages[`project${i}`].title = titles[i];
+  pages[`project${i}`].description = descriptions[i];
+  pages[`project${i}`].technology = technologies[i];
+
+}
+
+console.log(pages)
+
+
