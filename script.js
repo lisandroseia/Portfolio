cd72580e39b7440f86eb8c6ce67d4ecb2adf2849
lisandroseia <lisandroseia11@gmail.com>
2021-12-14 12:14:34 -0300

    Fix linters

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const menu = document.querySelector(".nav-ul");
 const menuItem = document.querySelectorAll(".nav-li");
 
 menuButton.addEventListener("click", () => {
-  const visibility = menu.getAttribute("data-visible");
-  if (visibility === "false") {
-    menu.setAttribute("data-visible", true);
+  const visibility = menu.getAttribute('data-visible');
+  if (visibility === 'false') {
+    menu.setAttribute('data-visible', true);
     menuButton.setAttribute("aria-expanded", true);
   } else {
-    menu.setAttribute("data-visible", false);
-    menuButton.setAttribute("aria-expanded", false);
+    menu.setAttribute('data-visible', false);
+    menuButton.setAttribute('aria-expanded', false);
   }
 });
 
 menuItem.forEach((item) => {
   item.addEventListener("click", () => {
-    const visibility = menu.getAttribute("data-visible");
-    if (visibility === "false") {
-      menu.setAttribute("data-visible", true);
-      menuButton.setAttribute("aria-expanded", true);
-    } else {
-      menu.setAttribute("data-visible", false);
-      menuButton.setAttribute("aria-expanded", false);
-    }
+    const visibility = menu.getAttribute('data-visible');
+  if (visibility === 'false') {
+    menu.setAttribute('data-visible', true);
+    menuButton.setAttribute("aria-expanded", true);
+  } else {
+    menu.setAttribute('data-visible', false);
+    menuButton.setAttribute('aria-expanded', false);
+  }
   });
 });
