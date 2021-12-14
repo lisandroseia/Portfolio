be234676e149a695740f33df3123276310c82888
lisandroseia <lisandroseia11@gmail.com>
2021-12-14 12:02:45 -0300

    Implement arrow functions

--- script.js
+++ script.js
@@ -2,13 +2,7 @@ const menuButton = document.querySelector('.hamburger-btn');
 const menu = document.querySelector('.nav-ul');
 const menuItem = document.querySelectorAll('.nav-li');
 
-menuButton.addEventListener('click', toggleMenu)
-
-menuItem.forEach((item) => {
-  item.addEventListener('click', toggleMenu)
-})
-
-function toggleMenu(){
+menuButton.addEventListener('click', () => {
   const visibility = menu.getAttribute('data-visible');
   if(visibility==="false"){
     menu.setAttribute('data-visible', true);
@@ -17,4 +11,17 @@ function toggleMenu(){
     menu.setAttribute('data-visible', false);
     menuButton.setAttribute('aria-expanded', false)
   }
-}
+})
+
+menuItem.forEach((item) => {
+  item.addEventListener('click', () => {
+    const visibility = menu.getAttribute('data-visible');
+  if(visibility==="false"){
+    menu.setAttribute('data-visible', true);
+    menuButton.setAttribute('aria-expanded', true)
+  } else {
+    menu.setAttribute('data-visible', false);
+    menuButton.setAttribute('aria-expanded', false)
+  }
+  })
+})
