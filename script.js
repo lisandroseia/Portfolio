03866ff631900aba221eaaf01510bbf133ed7f1f
lisandroseia <lisandroseia11@gmail.com>
2021-12-14 12:22:09 -0300

    Config pretier to match linters

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const menuButton = document.querySelector(".hamburger-btn");
-const menu = document.querySelector(".nav-ul");
-const menuItem = document.querySelectorAll(".nav-li");
+const menuButton = document.querySelector('.hamburger-btn');
+const menu = document.querySelector('.nav-ul');
+const menuItem = document.querySelectorAll('.nav-li');
 
-menuButton.addEventListener("click", () => {
+menuButton.addEventListener('click', () => {
   const visibility = menu.getAttribute('data-visible');
-    if (visibility === 'false') {
-      menu.setAttribute('data-visible', true);
-      menuButton.setAttribute('aria-expanded', true);
-    } else {
-      menu.setAttribute('data-visible', false);
-      menuButton.setAttribute('aria-expanded', false);
-    }
+  if (visibility === 'false') {
+    menu.setAttribute('data-visible', true);
+    menuButton.setAttribute('aria-expanded', true);
+  } else {
+    menu.setAttribute('data-visible', false);
+    menuButton.setAttribute('aria-expanded', false);
+  }
 });
 
 menuItem.forEach((item) => {
