60c5be71174abcab94b809fd8fa09aca914c3085
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2021-12-16 21:46:42 +0200

    Prevent undefined values

--- script.js
+++ script.js
@@ -212,22 +212,34 @@ submit.addEventListener('click', (e) => {
 
 /* data storage */
 
-
 const emailField = document.querySelector('#email');
 const nameField = document.querySelector('#name');
 const textField = document.querySelector('#text');
 const storeds = document.querySelectorAll('.stored');
-let obj = {};
+const obj = {};
 
+storeds.forEach((item) => {
+  item.addEventListener('input', () => {
+    obj.email = emailField.value;
 
-storeds.forEach(item => {
-item.addEventListener('input', () => {
-obj.email = emailField.value;
+    obj.name = nameField.value;
 
-obj.name = nameField.value;
+    obj.textarea = textField.value;
 
-obj.textarea = textField.value;
+    localStorage.setItem('form', JSON.stringify(obj));
+  });
+});
 
-localStorage.setItem('form', JSON.stringify(obj));
-})
-}); 
+const data = JSON.parse(localStorage.getItem('form'));
+
+window.onload = () => {
+  if (data.name !== undefined) {
+    nameField.value = data.name;
+  }
+  if (data.email !== undefined) {
+    emailField.value = data.email;
+  }
+  if (data.textarea !== undefined) {
+    textField.value = data.textarea;
+  }
+};
